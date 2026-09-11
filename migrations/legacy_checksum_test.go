c312ff58792fe3ac057fd5c31415ee691aacb548
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-09-11 12:43:42 +0300

    style: gofmt legacy migration checksum test

--- migrations/legacy_checksum_test.go
+++ migrations/legacy_checksum_test.go
@@ -12,7 +12,7 @@ func TestV031MigrationChecksumsRemainImmutable(t *testing.T) {
 		"0001_initial.up.sql":                         "d672eacc8d416910a91985ceef669c94e5c13d95afccbadbf4b51dbc91f51d7a",
 		"0002_local_identity_rbac_audit.up.sql":       "13813436248b037d18e75de644c1969120cc737311e295096664224e1c223f49",
 		"0003_identity_persistence_invariants.up.sql": "7c2ad4d2c44fa77004adecb7609c91c22aa58b3ba5ba84947160dd101b86a2a1",
-		"0004_change_execution_core.up.sql":            "6fbecffabd0716e07842d8843daa5ff87b0700ba773fbafb3df77697570bccc1",
+		"0004_change_execution_core.up.sql":           "6fbecffabd0716e07842d8843daa5ff87b0700ba773fbafb3df77697570bccc1",
 	}
 
 	for name, want := range expected {
